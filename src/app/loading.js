4cd569dbf45970a0bf6cb97d047d93b00cabe19c
nhidalgoCyD <nhidalgo@cyd-tec.com>
2024-05-08 09:58:29 -0400

    ADD: loading view

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+'use client'
+
+import LoadPage from "@/components/LoadPage";
+
+function Loading() {
+  return (
+    <LoadPage label={"Cargando..."} />
+  );
+}
+
+export default Loading;
--- src/components/LoadPage.js
+++ src/components/LoadPage.js
@@ -1,6 +1,6 @@
 import { Grid, LinearProgress, Typography } from "@mui/material";
-import logo from "/public/assets/images/cydocs-transparente.webp";
 import Image from "next/image";
+import logo from "/public/assets/images/cydocs-transparente.webp";
 
 function LoadPage({ label }) {
   return (
@@ -12,33 +12,49 @@ function LoadPage({ label }) {
       spacing={2}
       sx={{ mt: "10vh" }}
     >
-      <Grid item xs={6}>
+      <Grid item xs={12} sx={{ mx: "10%" }}>
         <Image
           style={{
-            height: "43vh",
+            height: "20vh",
+            width: "auto",
             display: "flex",
             alignItems: "center",
             marginLeft: "auto",
             marginRight: "auto",
-            width: "auto",
           }}
           src={logo}
           priority={true}
           alt="logo"
         />
-        {label && (
-          <Grid container justifyContent="center" alignItems="center">
-            <Grid item>
+      </Grid>
+      <Grid item xs={12} sx={{ mx: "20%" }}>
+        <Grid
+          container
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+        >
+          {label && (
+            <Grid item xs={12}>
               <Typography variant="h3" sx={{ pb: 2 }}>
                 {label}
               </Typography>
             </Grid>
+          )}
+          <Grid
+            item
+            xs={12}
+            sx={{
+              width: "100%",
+              textAlign: "center",
+              marginLeft: "auto",
+              marginRight: "auto",
+              maxWidth: "500px !important",
+            }}
+          >
+            <LinearProgress color="primary"></LinearProgress>
           </Grid>
-        )}
-        <LinearProgress
-          color="primary"
-          style={{ marginLeft: "10vh", marginRight: "10vh" }}
-        ></LinearProgress>
+        </Grid>
       </Grid>
     </Grid>
   );
